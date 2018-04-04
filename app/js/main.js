5640b86986fe301437b3755efa5568a144c5b4ca
Jose Perez <joseluis@watson.pe>
2018-04-04 12:05:23 -0500

    Layout homepage

---
+++ app/js/main.js
@@ -0,0 +1,54 @@
+"use strict";
+
+/*******************************************************
+function verifyMedia () {
+  if (window.matchMedia("(max-width: 991px)").matches) {
+
+  } else {
+    console.log('Mi viewport es mayor o igual a 992px');
+  }
+}
+/*******************************************************/
+
+$(function () {
+  var $window = $(window),
+      $document = $(document);
+
+  $window.on('scroll', function (ev) {
+    var arrow = $('.ArrowTop');
+
+    if ( $(this).scrollTop() > 150 ) {
+      arrow.fadeIn();
+    } else {
+      arrow.fadeOut();
+    }
+  });
+
+  $('.ArrowTop').on('click', function (ev) {
+    ev.preventDefault();
+
+    $('html, body').animate({scrollTop: 0}, 800);
+  });
+
+  //$document.on('ready', function () {
+  //});
+
+  $('.element-animate').hover(
+    function () {
+      var $this = $(this),
+          animation = $this.data('animation');
+
+      $(this).addClass('animated ' + animation);
+    },
+    function () {
+      var $this = $(this),
+          animation = $this.data('animation');
+
+      $(this).removeClass('animated ' + animation);
+    }
+  );
+
+  //$window.on('resize',  function () {
+  //  verifyMedia();
+  //});
+});
